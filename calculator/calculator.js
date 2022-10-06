8435a7c23065bde830e2e7cdcd5191a6d62c63f3
brandon <brandonkwamou@gmail.com>
2022-10-06 08:51:02 +0200

    bmi calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -7,8 +7,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
+app.post("/bmicalculator", (req, res) => {
+	let weight = parseFloat(req.body.weight);
+	let height = parseFloat(req.body.height);
+	let result = weight / ((height * height) / 100);
+	res.send(`Your bmi score is ${(result * 100).toFixed(2)}`);
+});
+
+app.get("/bmicalculator", (req, res) => {
+	res.sendFile(`${__dirname}/bmiCalculator.html`);
+});
+
 app.post("/", (req, res) => {
-	let result = parseInt(req.body.num1) + parseInt(req.body.num2);
+	let num1 = Number(req.body.num1);
+	let num2 = Number(req.body.num2);
+	let result = num1 + num2;
 	res.send(`The result of the calculation is ${result}`);
 });
 
