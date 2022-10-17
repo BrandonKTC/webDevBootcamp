6c6f9ee14a0842da93f61ec7c3c3550c89d3da24
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-10-17 19:28:04 +0200

    starter setup

--- secret/app.js
+++ secret/app.js
@@ -1,1 +1,27 @@
-//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.get("/register", (req, res) => {
+	res.render("register");
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on : http:localhost:${process.env.PORT}`);
+});
